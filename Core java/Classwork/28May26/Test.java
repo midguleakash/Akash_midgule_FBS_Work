111f4a87bbac32ee3f634df40a9d8ae0fcd41432
midguleakash <midguleakash@gmail.com>
2026-05-28 22:08:25 +0530

    Solve Java class and object creation example

---
+++ Core java/Classwork/28May26/Test.java
@@ -0,0 +1,25 @@
+class Date {
+	int day ;
+	int month;
+	int year;	
+
+} //Date class date end here
+
+
+class Test{
+	public static void main(String args[]){
+
+		Date d1 ; // references
+		d1 = new Date();
+		System.out.println(d1);
+		
+		Date d2 ; // references
+		d2 = new Date();
+		System.out.println(d2);
+
+
+
+	}
+}//Test class date end here
+
+
