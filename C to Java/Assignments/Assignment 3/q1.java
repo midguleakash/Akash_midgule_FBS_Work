2df634f506c71696545249b4b3231b4340474c61
midguleakash <midguleakash@gmail.com>
2026-06-02 09:48:13 +0530

    solve c to java assignment

---
+++ C to Java/Assignments/Assignment 3/q1.java
@@ -0,0 +1,20 @@
+/* Print numbers from 1 to 10
+Output: 1 2 3 4 5 6 7 8 9 10 */
+
+
+import java.util.Scanner;
+
+public class q1 {
+
+	public static void main(String args[]){
+
+			
+		int n = 1 ;
+	
+		while(n <= 10) {
+			System.out.printf("%d  ", n);
+			n++ ;
+		}
+	}
+}
+
