3d2fb32cd21ddae9572ee1b1bd6e9b310740d846
midguleakash <midguleakash@gmail.com>
2026-08-25 09:47:24 +0530

    implement basic servlet

--- Advanced Java/Workspace Advanced Java/ServletDemo/src/main/java/servlet/LoginServlet.java
+++ Advanced Java/Workspace Advanced Java/ServletDemo/src/main/java/servlet/LoginServlet.java
@@ -1,17 +1,31 @@
 package servlet;
 
-import java.io.IOException;
-
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/akash")
+public class LoginServlet extends HttpServlet {
+
+	
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	System.out.print("akash");
+    	String email = req.getParameter("email");
+    	String password = req.getParameter("password");
+    	PrintWriter pw = resp.getWriter();
+    	
+    	if(email.equals("firstbit") && password.equals("password")) {
+    		pw.print("login success");
+    	}else {
+    		pw.print("login fail");
+    	}
+    }
+    
+    
 }
